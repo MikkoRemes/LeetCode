b22ea4e253056969da622a14774f28b931f28560
Mikko Remes <mikko.remes@roimaint.com>
2025-02-18 12:41:32 +0200

    case 10 closer

--- 0010_RegularExpressionMatching/Program.cs
+++ 0010_RegularExpressionMatching/Program.cs
@@ -8,7 +8,9 @@ BenchmarkRunner.Run<Benchmarks>();
 public static class TestData
 {
     public record DataModel(string S, string A, bool IsMatch);
-    public static DataModel[] DataSets => [DataSet_01, DataSet_02, DataSet_03, DataSet_04, DataSet_05, DataSet_06, DataSet_07, DataSet_08, DataSet_09];
+    public static DataModel[] DataSets => 
+        [DataSet_01, DataSet_02, DataSet_03, DataSet_04, DataSet_05, DataSet_06, DataSet_07, DataSet_08, DataSet_09, DataSet_10,
+         DataSet_11, DataSet_12, DataSet_13, DataSet_14, DataSet_15, DataSet_16, DataSet_17, DataSet_18, DataSet_19];
 
     public static DataModel DataSet_01 = new("aa", "a", false);
     public static DataModel DataSet_02 = new("aa", "a*", true);
@@ -19,7 +21,16 @@ public static class TestData
     public static DataModel DataSet_07 = new("abbaacdc", "abba*acdc", true);
     public static DataModel DataSet_08 = new("abcd", "efg*", false);
     public static DataModel DataSet_09 = new("aab", "c*a*b", true);
-    public static DataModel DataSet_10 = new("mississippi", "mis*is*p*.", true);
+    public static DataModel DataSet_10 = new("mississippi", "mis*is*p*.", false);
+    public static DataModel DataSet_11 = new("aaa", "a*aa", true);
+    public static DataModel DataSet_12 = new("aaa", "a*aaa", true);
+    public static DataModel DataSet_13 = new("aaa", "a*aaaa", false);
+    public static DataModel DataSet_14 = new("baaa", "ba*aaaa", false);
+    public static DataModel DataSet_15 = new("ab", ".*c", false);
+    public static DataModel DataSet_16 = new("aaa", "aaaa", false);
+    public static DataModel DataSet_17 = new("aaa", ".*", true);
+    public static DataModel DataSet_18 = new("aaa", "**", true);
+    public static DataModel DataSet_19 = new("a", "ab*a", false);
 
     public static DataModel DataSet_long = new("abcdefghijabcdefghij", "a*bcdefghijab*cdefghij", true);
 }
@@ -54,6 +65,15 @@ public class Tests
     [InlineData(7)]
     [InlineData(8)]
     [InlineData(9)]
+    [InlineData(10)]
+    [InlineData(11)]
+    [InlineData(12)]
+    [InlineData(13)]
+    [InlineData(14)]
+    [InlineData(15)]
+    [InlineData(16)]
+    [InlineData(17)]
+    [InlineData(18)]
     public void Test_1(int testIndex) 
     {
         DataModel testData = TestData.DataSets[testIndex];
@@ -77,6 +97,18 @@ public class Tests
     [InlineData(4)]
     [InlineData(5)]
     [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
+    [InlineData(10)]
+    [InlineData(11)]
+    [InlineData(12)]
+    [InlineData(13)]
+    [InlineData(14)]
+    [InlineData(15)]
+    [InlineData(16)]
+    [InlineData(17)]
+    [InlineData(18)]
     public void Test_1RegEx(int testIndex)
     {
         DataModel testData = TestData.DataSets[testIndex];
@@ -100,6 +132,18 @@ public class Tests
     [InlineData(4)]
     [InlineData(5)]
     [InlineData(6)]
+    [InlineData(7)]
+    [InlineData(8)]
+    [InlineData(9)]
+    [InlineData(10)]
+    [InlineData(11)]
+    [InlineData(12)]
+    [InlineData(13)]
+    [InlineData(14)]
+    [InlineData(15)]
+    [InlineData(16)]
+    [InlineData(17)]
+    [InlineData(18)]
     public void Test_1ExampleSolution(int testIndex)
     {
         DataModel testData = TestData.DataSets[testIndex];
@@ -131,43 +175,48 @@ public class Solution
         int j = 0;
         for(int i = 0; i < p.Length; i++)
         {
+            if (i < p.Length - 1 && p[i + 1] == MATCHZEROORMORE)
+            {
+                if (p[i] == MATCHSINGLE)
+                    if (i == p.Length - 2)
+                        return true;
+                    else
+                    {
+                        i++;
+                        continue;
+                    }
 
-            if (p[i] == MATCHSINGLE)
+                while (j < s.Length && s[j] == p[i])
+                    j++;
+
+                for (int k = i + 2; k < p.Length; k++)
+                    if (p[k] == p[i])
+                        j--;
+                    else
+                        break;
+
+                if (j < 0)
+                    return false;
+
+                if (j >= s.Length)
+                    return true;// i < p.Length - 2;
+
+                i++;
+                continue;
+            }
+
+            else if (p[i] == MATCHSINGLE)
             {
                 j++;
                 continue;
             }
 
-            if (p[i] == MATCHZEROORMORE)
-            {
-                if (i == 0)
-                    return false;
-
-                if (p[i - 1] == MATCHSINGLE)
-                    return true;
-
-                while (j < s.Length)
-                    if (s[j] != p[i-1])
-                        break;
-                    else
-                        j++;
-
-                for (int k = i + 1; k < p.Length; k++)
-                    if (p[k] == p[i - 1])
-                        j--;
-                    else
-                        break;
-
-                continue;
-            }
-
-            if (s[j] != p[i])
-                if(i >= p.Length || p[i + 1] != MATCHZEROORMORE)
-                    return false;
+            if (j >= s.Length)
+                return false;
+            if (s[j] != p[i])                
+                return false;
 
             j++;
-            if (j > s.Length)
-                return false;
         }
 
         return j == s.Length;
